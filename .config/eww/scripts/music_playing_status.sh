2c549f0f1a80db6eb2e583c101acebe851c205e2
Astor730 <mister.p730@gmail.com>
2025-09-10 10:59:09 -0400

    removed eww for now

--- .config/eww/scripts/music_playing_status.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-STATUS="$(playerctl --player=spotify status 2>/dev/null)"
-
-if [ "$STATUS" = "Playing" ]; then
-    echo ""
-else 
-    echo "󰏦"
-fi
--- .config/eww/scripts/power_menu_toggle.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-
-eww close power_menu || (eww open power_menu)
