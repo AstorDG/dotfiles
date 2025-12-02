77f773350617f325f3bec7b881f236f0aa6c5d69
Astor730 <mister.p730@gmail.com>
2025-12-02 12:41:21 -0500

    Removed smear.nvim
    Removed the smear.nvim plugin bc I'm using the ghosty shadder now

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -902,16 +902,16 @@ require('lazy').setup({
 
   -- Plugin to show the cursor jumping around in the editor
   -- Helps readability for where the cursor is moving
-  {
-    'sphamba/smear-cursor.nvim',
-    opts = {
-      enabled = true,
-      smear_between_buffgers = false,
-      stiffness = 0.7,
-      trailing_stiffness = 0.5,
-      distance_stop_animating = 0.4,
-    },
-  },
+  -- {
+  --   'sphamba/smear-cursor.nvim',
+  --   opts = {
+  --     enabled = true,
+  --     smear_between_buffgers = false,
+  --     stiffness = 0.7,
+  --     trailing_stiffness = 0.5,
+  --     distance_stop_animating = 0.4,
+  --   },
+  -- },
 
   -- A powerful fuzzy finder that can look through buffers(files), the current buffer, documentation, LSPs and more
   -- has a nice ui with a popup window for better focusing.
