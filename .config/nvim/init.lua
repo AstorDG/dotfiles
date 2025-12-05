91ff885e1b1260006a3cb7180fd4333f0c5134af
Astor730 <mister.p730@gmail.com>
2025-12-05 10:04:15 -0500

    Updated my ghosty cursor
    Changed the cursor color to purple and the trail to glowing light
    purple. Also got rid of the color specifications in catppuccin bc of
    nvim colorizer.

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -318,37 +318,37 @@ require('lazy').setup({
           mocha = {
             -- Current Color #000000(Black)
             crust = '#FFFFFF',
-            -- Current Color #000000(Black). Color of the neo-tree background
+            -- Color of the neo-tree background
             mantle = '#000000',
-            -- Current Color #000000(Black). Color of the text editor background
+            -- Color of the text editor background
             base = '#000000',
-            -- Current Color #444444(Grey). Color of the current line highlight
+            -- Color of the current line highlight
             surface0 = '#444444',
-            -- Current Color #5c5c5c(Grey). Color of the background of currently highlighted text.
+            -- Color of the background of currently highlighted text.
             surface1 = '#5c5c5c',
-            -- Current color #33D4C4(Cyan). Unselected Tab name and folder lines.
+            -- Unselected Tab name and folder lines.
             overlay0 = '#D90202',
-            -- current color #69838C(grey cyan). Comments and periods/commas and parens.
+            -- Comments and periods/commas and parens, brackets and curly brackets.
             overlay2 = '#3379D4',
-            -- Current Color #33D4C4(cyan). Git branch and language in status line.
+            -- Git branch and language in status line.
             subtext1 = '#33D4C4',
-            -- Current color #33D4C4(cyan). File names, gitsigns, commands, file name on the status bar, indent_line context, indentation sign.
+            -- File names, gitsigns, commands, file name on the status bar, indent_line context, indentation sign.
             text = '#33D4C4',
-            -- Current color #D90202(brigth red). Folder color in neo-tree the background for normal mode the border for the buffers.
+            -- Folder color in neo-tree the background for normal mode the border for the buffers.
             blue = '#D90202',
-            -- Current color #D43394(bright pink). Yank and litagure color ->, =, <=
+            -- Yank and litagure color ->, =, <=
             sky = '#D43394',
-            -- Current color #FECEA0(tan). Insert mode, string, added color on the side bar
+            -- Insert mode, string, added color on the side bar
             green = '#FECEA0',
-            -- Current color #D90202(bright red). Color of git changes and unsaved files
+            -- Color of git changes and unsaved files
             yellow = '#D90202',
-            -- Current color #D90202(bright red). Booleans and integers
+            -- Booleans and integers
             peach = '#D90202',
-            -- Current color #3379D4(neon blue). Color of replace mode.
+            -- Color of replace mode.
             red = '#3379D4',
-            -- Current color #2CB03C(neon green). Visiual mode, new files, changed file folders, return and function keywords.
+            -- Visiual mode, new files, changed file folders, return and function keywords.
             mauve = '#2CB03C',
-            -- Current color #ffffce(light yellow). Curly braces
+            -- Not sure
             flamingo = '#2CB03C',
           },
         },
