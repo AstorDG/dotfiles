637d242506e9c695cfda42189c0f6abbd1553092
astordg <astormg999@gmail.com>
2026-08-29 13:45:26 -0400

    added nix-darwin to dotfiles

---
+++ nix-darwin-config/configuration.nix
@@ -0,0 +1,98 @@
+{ pkgs, ... }:
+
+{
+  # Replace with your macOS short username: `whoami`
+  system.primaryUser = "astor";
+
+  nixpkgs.hostPlatform = "aarch64-darwin";
+  # For an Intel Mac, use:
+  # nixpkgs.hostPlatform = "x86_64-darwin";
+  nixpkgs.config.allowUnfree = true;
+
+  nix.settings.experimental-features = [
+    "nix-command"
+    "flakes"
+  ];
+
+  environment.systemPackages = with pkgs; [
+
+  	##### Terminal Apps #####
+    fish
+    starship
+    btop
+    stow
+    nil
+    nixd
+    neovim
+    zig
+
+    ##### GUI Apps #####
+    zed-editor
+    ghostty-bin
+	raycast
+	obsidian
+	stirling-pdf-desktop
+	zoom-us
+	#zen-browser flake
+	inputs.zen-browser.packages.${pkgs.system}.default
+
+	##### Non Nix packages #####
+	#
+  ];
+
+  # Adds Fish to /etc/shells so macOS accepts it as a login shell.
+  programs.fish.enable = true;
+
+  # Makes Fish your login shell.
+  users.users.astor.shell = pkgs.fish;
+  environment.shells = [pkgs.fish];
+
+  # macOS settings managed by nix-darwin.
+  # Show hidden files in finder
+  system.defaults = {
+    NSGlobalDomain = {
+    	AppleEnableMouseSwipeNavigateWithScrolls = false;
+      AppleInterfaceStyle = "Dark";
+      AppleScrollerPagingBehavior = true;
+      AppleShowAllExtensions = true;
+      AppleShowAllFiles = true;
+      InitialKeyRepeat = 14;
+      KeyRepeat = 1;
+      NSAutomaticCapitalizationEnabled = false;
+      NSAutomaticPeriodSubstitutionEnabled = false;
+      NSAutomaticQuoteSubstitutionEnabled = false;
+      NSAutomaticSpellingCorrectionEnabled = false;
+      NSDocumentSaveNewDocumentsToCloud = false;
+    };
+
+    dock = {
+      autohide = true;
+      orientation = "left";
+    };
+
+    finder = {
+      AppleShowAllExtensions = true;
+      AppleShowAllFiles = true;
+      CreateDesktop = false;
+      FXDefaultSearchScope = "SCcf";
+      FXPreferredViewStyle = "clmv";
+      QuitMenuItem = true;
+      ShowPathbar = true;
+      _FXShowPosixPathInTitle = true;
+      _FXEnableColumnAutoSizing = true;
+      ShowStatusBar = true;
+    };
+
+    trackpad = {
+      Clicking = true;
+      TrackpadRightClick = true;
+    };
+  };
+
+  system.keyboard = {
+    enableKeyMapping = true;
+    remapCapsLockToEscape = true;
+  };
+
+  system.stateVersion = 7;
+}
---
+++ nix-darwin-config/flake.nix
@@ -0,0 +1,29 @@
+{
+  description = "Nix-Darwin Flake";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+
+    nix-darwin = {
+      url = "github:nix-darwin/nix-darwin/master";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+
+    zen-browser = {
+          url = "github:0xc000022070/zen-browser-flake";
+          inputs.nixpkgs.follows = "nixpkgs-unstable";
+    };
+  };
+
+  outputs =
+    inputs@{self,nix-darwin, nixpkgs, ... }:
+    {
+      darwinConfigurations."Astors-MacBook-Air" =
+     	nix-darwin.lib.darwinSystem {
+        	modules = [ ./configuration.nix ];
+	    };
+		specialArgs = {
+			inherit inputs;
+		};
+    };
+}
